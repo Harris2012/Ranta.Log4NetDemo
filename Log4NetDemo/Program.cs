6bf9e784751fc920765ef98c178c5ed8440060ec
HarrisZhang <harriszhang@live.cn>
2016-06-20 07:52:47 +0800

    init project

---
+++ Log4NetDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace TestLog4Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILog logger = LogManager.GetLogger(typeof(Program).FullName);
+
+            try
+            {
+                throw new Exception("This is a test.");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
